3fed1a6fb216352b92abdfeee6f93a681ab2a623
arunkumar <arunkumar.gudelli@gmail.com>
2021-12-16 15:16:17 +0530

    commiting changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatTableFilterComponent } from './mat-table-filter/mat-table-filter.component';
+import { MaterialComponentsModule } from './material-components/material-components.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MatTableFilterComponent } from './mat-table-filter/mat-table-filter.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material-components/material-components.module.ts
+++ src/app/material-components/material-components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [],
   imports: [
+    MatTableModule,
     CommonModule
+  ],
+  exports:[
+    MatTableModule
   ]
 })
 export class MaterialComponentsModule { }
